d8de4d74b500371cd2b25af06acdf216b278583b
Miriam Anyango <minaoanya@gmail.com>
2022-06-14 14:57:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+/*var customerName = function(customer){
+    return customer
+}
+console.log(customerName("bob"))*/
+
+var customerName = "bob"
+
+
+
+function upperCaseCustomerName(){
+    customerName = "BOB"
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+
+const leastFavoriteCustomer = 'miriam'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
